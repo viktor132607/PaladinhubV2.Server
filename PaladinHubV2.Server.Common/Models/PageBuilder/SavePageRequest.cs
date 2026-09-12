9a96034f03ae3286c24ed71834bfaf50753424d1
Viktor Iliev <96659685+viktor132607@users.noreply.github.com>
2026-09-12 18:51:48 +0300

    Extract page management request contract

---
+++ PaladinHubV2.Server.Common/Models/PageBuilder/SavePageRequest.cs
@@ -0,0 +1,10 @@
+namespace PaladinHub.Areas.Admin.Models
+{
+	public sealed class SavePageRequest
+	{
+		public string? Section { get; init; }
+		public string? Title { get; init; }
+		public string? Slug { get; init; }
+		public bool IsPublished { get; init; } = true;
+	}
+}
