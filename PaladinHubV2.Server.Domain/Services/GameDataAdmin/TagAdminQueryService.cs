71b10a4a3ec7461256e9165e1a3922f49369b928
Viktor Iliev <96659685+viktor132607@users.noreply.github.com>
2026-09-25 22:54:08 +0300

    Add TagAdminQueryService.cs

---
+++ PaladinHubV2.Server.Domain/Services/GameDataAdmin/TagAdminQueryService.cs
@@ -0,0 +1,55 @@
+using Microsoft.EntityFrameworkCore;
+using PaladinHubV2.Server.Common.Models.GameData;
+using PaladinHubV2.Server.Data;
+using PaladinHubV2.Server.Data.Entities;
+
+namespace PaladinHubV2.Server.Domain.Services.GameDataAdmin;
+
+public sealed class TagAdminQueryService :
+    ITagAdminQueryService
+{
+    private readonly AppDbContext _db;
+
+    public TagAdminQueryService(
+        AppDbContext db)
+    {
+        _db = db;
+    }
+
+    public Task<List<TagListItem>> ListAsync(
+        CancellationToken cancellationToken)
+    {
+        return _db.GameTages
+            .AsNoTracking()
+            .OrderBy(item => item.SortOrder)
+            .ThenBy(item => item.Name)
+            .Select(item => new TagListItem(
+                item.Id,
+                item.Name,
+                item.Description,
+                null,
+                item.SortOrder,
+                item.IsArchived,
+                item.IsDeleted,
+                item.Version,
+                _db.Spells.Count(spell =>
+                    spell.TagIds.Contains(item.Id)) +
+                _db.Items.Count(product =>
+                    product.TagIds.Contains(item.Id)),
+                0))
+            .ToListAsync(cancellationToken);
+    }
+
+    public Task<List<TagRevision>> HistoryAsync(
+        int id,
+        CancellationToken cancellationToken)
+    {
+        return _db.TagRevisions
+            .AsNoTracking()
+            .Where(revision =>
+                revision.TagId == id)
+            .OrderByDescending(revision =>
+                revision.Version)
+            .ToListAsync(cancellationToken);
+    }
+}
